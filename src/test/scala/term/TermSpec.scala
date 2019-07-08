601363a298c91dcb9bd6c914cf8413e92617c95c
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-07-09 07:43:34 +0900

    chotto test tuika

--- src/test/scala/term/TermSpec.scala
+++ src/test/scala/term/TermSpec.scala
@@ -4,13 +4,25 @@ import org.scalatest._
 
 class TermSpec extends FlatSpec with Matchers {
 
-  "The evaluation static trace" should "be correct" in {
+  "The evaluation static trace" should "be correct(1)" in {
     val term = If(IsZero(Pred(Succ(Zero))), True, False)
     val stackTrace: List[Term] =
       If(IsZero(Pred(Succ(Zero))), True, False) ::
       If(IsZero(Zero), True, False) ::
       If(True, True, False) ::
       True ::
+      Nil
+
+    term.evalStackTrace shouldEqual stackTrace
+  }
+
+  "The evaluation static trace" should "be correct(2)" in {
+    val term = Pred(Pred(Pred(Succ(Succ(Succ(Succ(Zero)))))))
+    val stackTrace: List[Term] =
+      Pred(Pred(Pred(Succ(Succ(Succ(Succ(Zero))))))) ::
+      Pred(Pred(Succ(Succ(Succ(Zero))))) ::
+      Pred(Succ(Succ(Zero))) ::
+      Succ(Zero) ::
       Nil
 
     term.evalStackTrace shouldEqual stackTrace
