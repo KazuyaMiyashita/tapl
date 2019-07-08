38aab14def27d470abc520d2376b17abf743c468
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-07-09 06:00:19 +0900

    wip

---
+++ src/test/scala/term/SuccSpec.scala
@@ -0,0 +1,23 @@
+package term
+
+import org.scalatest._
+
+class SuccSpec extends FlatSpec with Matchers {
+
+  "succ t1" should "be succ t1'a when t1 -> t1'" in {
+    val term1 = IsZero(Zero)
+    val term1a = True
+    term1.eval shouldEqual term1a
+
+    Succ(term1).evalOnce shouldEqual Succ(term1a)
+  }
+
+  "succ zero" should "be normal form" in {
+    Succ(Zero).isNormalForm shouldEqual true
+  }
+
+  "succ (iszero (zero))" should "be not normal form" in {
+    Succ(IsZero(Zero)).isNormalForm shouldEqual false
+  }
+
+}
