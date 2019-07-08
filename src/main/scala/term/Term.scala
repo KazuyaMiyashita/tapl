ab779a8039b4c681aa5b370e4380fe405ab612dd
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-07-09 07:37:28 +0900

    evalStackTrace

--- src/main/scala/term/Term.scala
+++ src/main/scala/term/Term.scala
@@ -4,6 +4,13 @@ sealed trait Term {
   def evalOnce: Term
   def isNormalForm: Boolean
   def eval: Term = if (isNormalForm) this else evalOnce.eval
+
+  def evalStackTrace: List[Term] = {
+    def proc(term: Term, acc: List[Term]): (Term, List[Term]) = 
+      if (term.isNormalForm) (term, term :: acc)
+      else proc(term.evalOnce, term :: acc)
+    proc(this, Nil)._2.reverse
+  }
 }
 
 trait Value extends Term {
@@ -19,7 +26,7 @@ case class If(cond: Term, onTrue: Term, onFalse: Term) extends Term {
   override def evalOnce: Term = cond match {
     case True => onTrue
     case False => onFalse
-    case cond => If(cond.eval, onTrue, onFalse)
+    case cond => If(cond.evalOnce, onTrue, onFalse)
   }
   override def isNormalForm = false
 }
@@ -28,13 +35,13 @@ case class IsZero(term: Term) extends Term {
   override def evalOnce: Term = term match {
     case Zero => True
     case Succ(t: Term) => False
-    case term => IsZero(term.eval)
+    case term => IsZero(term.evalOnce)
   }
   override def isNormalForm = false
 }
 
 case class Succ(term: Term) extends Term {
-  override def evalOnce: Term = Succ(term.eval)
+  override def evalOnce: Term = Succ(term.evalOnce)
   override def isNormalForm: Boolean = term.isNormalForm
 }
 
@@ -42,7 +49,7 @@ case class Pred(term: Term) extends Term {
   override def evalOnce: Term = term match {
     case Zero => Zero
     case Succ(t: Term) => t
-    case term => Pred(term.eval)
+    case term => Pred(term.evalOnce)
   }
   override def isNormalForm: Boolean = term match {
     case Zero => true
--- src/test/scala/term/TermSpec.scala
+++ src/test/scala/term/TermSpec.scala
@@ -4,4 +4,16 @@ import org.scalatest._
 
 class TermSpec extends FlatSpec with Matchers {
 
+  "The evaluation static trace" should "be correct" in {
+    val term = If(IsZero(Pred(Succ(Zero))), True, False)
+    val stackTrace: List[Term] =
+      If(IsZero(Pred(Succ(Zero))), True, False) ::
+      If(IsZero(Zero), True, False) ::
+      If(True, True, False) ::
+      True ::
+      Nil
+
+    term.evalStackTrace shouldEqual stackTrace
+  }
+
 }
