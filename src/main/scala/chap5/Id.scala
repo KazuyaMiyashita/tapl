87fc5cbee58ff8c0cd682ec16c86be0a9766b952
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-07-15 22:01:18 +0900

    gomi

--- src/main/scala/chap5/Id.scala
+++ src/main/scala/chap5/Id.scala
@@ -1,4 +1,27 @@
 package chap5
 
+import chap3._
+
 case class Id(variable: Variable, term: Term)
   extends Lambda(variable, variable)
+
+case object Factorial extends Lambda(
+  X,
+  If(IsZero(X), Succ(Zero), Multiple(X, Factorial(Pred(X))))
+)
+
+case object Tru extends Lambda(
+  X,
+  Lambda(
+    Y,
+    X
+  )
+)
+
+case object Fls extends Lambda(
+  X,
+  Lambda(
+    Y,
+    Y
+  )
+)
--- src/main/scala/chap5/Term.scala
+++ src/main/scala/chap5/Term.scala
@@ -3,5 +3,5 @@ package chap5
 sealed trait Term
 
 trait Variable extends Term
-abstract class Lambda(variable: Variable, term: Term)
+abstract class Lambda(term: Term)
 case class Applicate(term1: Term, term2: Term)
